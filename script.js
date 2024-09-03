75eebeafe5b6f8e88c81113daaaf84e53a580dee
Matias <mmellamella2@gmail.com>
2024-09-02 22:30:26 -0400

    scrit

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 function buscarPokemon(contenedorNumero){
     let inputId = `pokemonimput${contenedorNumero}`;
     let nombrePokemon = document.getElementById(inputId).value.trim().toLowerCase();
+    let urlApi = `https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`
 
-    console.log(inputId)
-
+    fetch(urlApi)
+    .then(Response => Response.json())
+    .then(datosPokemon => mostrarPokemon(datosPokemon,contenedorNumero))
+    .catch(()=> mostrarError(contenedorNumero))   
 }
+//Mostrar informacion del Pokemon
